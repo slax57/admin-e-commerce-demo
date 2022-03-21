74b9ce5ddf8652ea012590ecdbfcd195ae92bda8
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-03-21 16:53:55 +0100

    add search and filters

--- src/command/CommandList.tsx
+++ src/command/CommandList.tsx
@@ -5,16 +5,32 @@ import {
   ReferenceField,
   NumberField,
   DateField,
-  ArrayField,
-  SingleFieldList,
-  ChipField,
   FunctionField,
+  TextInput,
+  ReferenceInput,
+  AutocompleteInput,
+  DateInput,
+  NumberInput,
+  NullableBooleanInput,
 } from "react-admin";
 import CustomerAddressField from "../customer/CustomerAddressField";
 import CustomerField from "../customer/CustomerField";
 
+const filters = [
+  <TextInput source="q" label="Search" alwaysOn resettable />,
+  <ReferenceInput source="customer_id" label="Customer" reference="customers">
+    <AutocompleteInput
+      optionText={(record) => `${record.first_name} ${record.last_name}`}
+    />
+  </ReferenceInput>,
+  <DateInput label="Passed Since" source="date_gte" />,
+  <DateInput label="Passed Before" source="date_lte" />,
+  <NumberInput label="Min amount" source="total_gte" />,
+  <NullableBooleanInput label="Returned" source="returned" />,
+];
+
 export const CommandList = () => (
-  <List resource="commands" title="Orders">
+  <List resource="commands" title="Orders" filters={filters}>
     <Datagrid rowClick="edit">
       <DateField source="date" showTime />
       <TextField source="reference" />
