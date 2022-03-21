0b21c8e6deb19e8d4debeec3404213b1cb2c11e2
Jean-Baptiste Kaiser <jb@marmelab.com>
2022-03-21 18:31:12 +0100

    WIP: add tabbed status filter

--- src/command/CommandList.tsx
+++ src/command/CommandList.tsx
@@ -1,3 +1,4 @@
+import { Box, Tabs, Tab } from "@mui/material";
 import {
   List,
   Datagrid,
@@ -15,6 +16,8 @@ import {
 } from "react-admin";
 import CustomerAddressField from "../customer/CustomerAddressField";
 import CustomerField from "../customer/CustomerField";
+import { useSearchParams } from "react-router-dom";
+import { useCallback } from "react";
 
 const filters = [
   <TextInput source="q" label="Search" alwaysOn resettable />,
@@ -29,31 +32,61 @@ const filters = [
   <NullableBooleanInput label="Returned" source="returned" />,
 ];
 
-export const CommandList = () => (
-  <List resource="commands" title="Orders" filters={filters}>
-    <Datagrid rowClick="edit">
-      <DateField source="date" showTime />
-      <TextField source="reference" />
-      <ReferenceField source="customer_id" reference="customers">
-        <CustomerField />
-      </ReferenceField>
-      <ReferenceField
-        source="customer_id"
-        reference="customers"
-        link={false}
-        label="Address"
-      >
-        <CustomerAddressField />
-      </ReferenceField>
-      <FunctionField
-        label="Nb items"
-        render={(record: { basket: any[] }) => `${record.basket?.length}`}
-        textAlign="right"
-      />
-      <NumberField
-        source="total"
-        options={{ style: "currency", currency: "USD" }}
-      />
-    </Datagrid>
-  </List>
-);
+export const CommandList = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const onTabChange = useCallback(
+    (_, value) => {
+      const updatedSearchParams = new URLSearchParams(searchParams.toString());
+      updatedSearchParams.set("status", value);
+      setSearchParams(updatedSearchParams.toString());
+    },
+    [searchParams, setSearchParams]
+  );
+  const currentTab = useCallback(() => {
+    return searchParams.get("status") || "ordered";
+  }, [searchParams]);
+
+  return (
+    <List
+      resource="commands"
+      title="Orders"
+      filters={filters}
+      filter={{ status: currentTab() }}
+    >
+      <>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={currentTab()} onChange={onTabChange} variant="fullWidth">
+            <Tab label="Ordered" value="ordered" />
+            <Tab label="Delivered" value="delivered" />
+            <Tab label="Cancelled" value="cancelled" />
+          </Tabs>
+        </Box>
+        <Datagrid rowClick="edit">
+          <DateField source="date" showTime />
+          <TextField source="reference" />
+          <ReferenceField source="customer_id" reference="customers">
+            <CustomerField />
+          </ReferenceField>
+          <ReferenceField
+            source="customer_id"
+            reference="customers"
+            link={false}
+            label="Address"
+          >
+            <CustomerAddressField />
+          </ReferenceField>
+          <FunctionField
+            label="Nb items"
+            render={(record: { basket: any[] }) => `${record.basket?.length}`}
+            textAlign="right"
+          />
+          <NumberField
+            source="total"
+            options={{ style: "currency", currency: "USD" }}
+          />
+        </Datagrid>
+      </>
+    </List>
+  );
+};
